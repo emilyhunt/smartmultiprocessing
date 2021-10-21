1cac8b66b3cf913702a5d25e77aacf73f4ed0211
Emily Hunt <emily.hunt.physics@gmail.com>
2021-10-21 22:22:21 +0200

    Hopefully a final version of the virial ratio cut!

--- utilities.py
+++ utilities.py
@@ -28,16 +28,24 @@ class Logfile:
 
         self.logfile = logfile
         self.pipe = pipe
+        self.pipe_on = True
+        self.logfile_on = True
 
     def __call__(self, message: str, send_to_pipe: bool = True,
                  send_to_logfile: bool = True, send_to_print: bool = False):
 
-        if send_to_pipe and self.pipe is not None:
+        if send_to_pipe and self.pipe is not None and self.pipe_on:
             self.pipe.send(timestamp(date=False) + message)
 
-        if send_to_logfile:
+        if send_to_logfile and self.logfile_on:
             with open(self.logfile, 'a') as file:
                 file.write(timestamp() + message + "\n")
 
         if send_to_print:
             print(timestamp() + message)
+
+    def set_pipe_on(self, state: bool):
+        self.pipe_on = state
+
+    def set_logfile_on(self, state: bool):
+        self.logfile_on = state
